f2529f9673e8a923cab2a749f48400fcbc51e993
Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>
2024-10-23 18:59:41 +0545

    added required method for smartcontract integration

--- src/components/home-page/trade/modals/AllowanceModal.tsx
+++ src/components/home-page/trade/modals/AllowanceModal.tsx
@@ -5,18 +5,22 @@ interface IAllowanceModal {
    isOpen: boolean
    onApprove: () => void
    onClose: () => void
+   transactionHash?: string | null // Add transactionHash prop
 }
 
-const AllowanceModal: React.FC<IAllowanceModal> = ({ isOpen, onApprove, onClose }) => {
+const AllowanceModal: React.FC<IAllowanceModal> = ({ isOpen, onApprove, onClose, transactionHash }) => {
    return (
-      <ModalWrapper
-         confirmButtonTitle="Approve"
-         onClose={onClose}
-         onConfirm={onApprove}
-         isOpen={isOpen}
-         title="Would you like  to give an allowance ?"
-      >
-         <div></div>
+      <ModalWrapper confirmButtonTitle="Approve" onClose={onClose} onConfirm={onApprove} isOpen={isOpen} title="Grant Allowance">
+         <div>By approving, you will grant the necessary allowance for the MultiKeeper Factory contract to create MultiKeeper Contract.</div>
+
+         {transactionHash && (
+            <div className="mt-4">
+               <strong>Transaction Hash:</strong>{" "}
+               <a href={`https://etherscan.io/tx/${transactionHash}`} target="_blank" rel="noopener noreferrer">
+                  {transactionHash}
+               </a>
+            </div>
+         )}
       </ModalWrapper>
    )
 }
