18a8a9ef3f338c37ae566524f828dd082ea40fac
Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>
2024-11-12 16:11:53 +0545

    updated keeperFactoryAddress

--- src/constants/blockchain.ts
+++ src/constants/blockchain.ts
@@ -8,7 +8,7 @@ export type LinkToken = {
    decimal: string
 }
 
-export const multiTokenKeeperFactoryAddress = "0x3f999C7A087F9c45cac76732dAc6aCd95E15759F"
+export const multiTokenKeeperFactoryAddress = "0xC98b425B1681009b703d8766B0DE022Ce2D6b6cb"
 export const linkTokenAddress: string = "0xE4aB69C077896252FAFBD49EFD26B5D171A32410"
 export const maxApproveAmount = ethers.parseUnits("10000000000000000000000000000000", 18)
 export const defaultApproveAmount = ethers.parseUnits("10000000000000000000000000000000", 18)
